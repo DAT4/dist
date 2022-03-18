9d8fcb488d0424ab62fd58757531d0cc6b886f4c
Martin Maartensson <mail@mama.sh>
2022-03-18 20:53:25 +0100

    ok

--- main.c
+++ main.c
@@ -1,58 +1,64 @@
-#include "mpi.h"
-#include "time.h"
+#include <omp.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define  MASTER		0
+#include <errno.h>
+#include <limits.h>
+#include <string.h> 
+#define  MASTER 0
 #define PI25DT 3.141592653589793238462643
 #define INTERVALS 10000000
 
+double PI();
 
 int main (int argc, char *argv[])
 {
-    double x, f, local_sum, pi;
+	if (strlen(argv[1]) == 0) {return 1;}
+	char* p;
+	errno = 0;
+	long arg = strtol(argv[1], &p, 10);
+	if (*p != '\0' || errno != 0) {return 1;}
+	if (arg < INT_MIN || arg > INT_MAX) {return 1;}
+	int cpu = arg;
+	double itime, ftime, exec_time;
+	itime = omp_get_wtime();
 
-    double dx = 1.0 / (double) INTERVALS;
+	double global_sum, pi, dx;
 
-    int  numtasks, taskid, len, partner, message;
+	dx = 1.0 / (double) INTERVALS;
 
+#	pragma omp parallel num_threads(cpu) reduction(+: global_sum)
+	global_sum += PI(&global_sum);
 
-    MPI_Status status;
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
-
-    long int intervals = INTERVALS/numtasks;
-    long int start = intervals * (int) (taskid+1);
-    long int stop = start - intervals;
-    time_t time1;
-    double time2;
-
-
-    if (taskid == 0) { 
-	time1 = clock();
-	//printf("Number of intervals pr core: %ld\n", intervals); 
-    }
-
-    local_sum = 0.0;
-    for (int i = start; i > stop ; i--) {
-	x = dx * ((double) (i - 0.5));
-	local_sum = local_sum + 4.0 / (1.0 + x*x);
-    }
-
-    double global_sum;
-
-    MPI_Reduce(&local_sum, &global_sum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-
-    if (taskid == 0) {
-	time2 = (clock() - time1) / (double) CLOCKS_PER_SEC;
 	pi = dx*global_sum;
-	printf("%d, %.24f, %.24f, %.24f, %.24f\n", numtasks, pi, PI25DT, PI25DT-pi, time2);
-    }
 
-    MPI_Finalize();
+	ftime = omp_get_wtime();
+	exec_time = ftime - itime;
 
-    return 0;
+	printf("%d, %.24f, %.24f, %.24f\n", cpu, pi, PI25DT-pi, exec_time);
+
+	return 0;
+}
+
+double PI()
+{
+	double x;
+	double dx = 1.0 / (double) INTERVALS;
+
+	int numtasks = omp_get_num_threads();
+	int taskid = omp_get_thread_num();
+	int intervals = INTERVALS/numtasks;
+
+	int start = intervals * (int) (taskid+1);
+	int stop = start - intervals;
+
+	double local_sum = 0.0;
+
+	for (int i = start; i > stop ; i--) {
+		x = dx * ((double) (i - 0.5));
+		local_sum = local_sum + 4.0 / (1.0 + x*x);
+	}
+
+	return local_sum;
 }
 
